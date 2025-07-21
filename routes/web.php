be439c4fc10864a0d1fbc985c55efcc8bd5b09f8
WahidMuhammadDjati <wahiddjati5@gmail.com>
2025-07-21 16:21:51 +0700

    pass login

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\AuthController;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::get('/', function () {
+Route::get('/index', function () {
     return view('siswa.index');
 });
 Route::resource('siswa', SiswaController::class);
@@ -15,6 +15,6 @@ Route::get('/', [SiswaController::class, 'index']);
 
 
 
-Route::get('/login', [AuthController::class, 'loginForm'])->name('login');
+Route::get('/', [AuthController::class, 'loginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
